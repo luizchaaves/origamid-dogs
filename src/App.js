b5f6fa0ad66c30dd6fa5a64afa6dcb554d7948f2
Luiz Otavio <luizotavio0106@outlook.com>
2022-09-08 16:17:16 -0300

    feat/creating a protected route to the user's page when logged in

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
+import User from './Components/User/User';
 import { UserStorage } from './UserContext';
 
 import './App.css';
+import ProtectedRoute from './Components/Helper/ProtectedRoute';
 
 function App() {
   return (
@@ -16,7 +18,15 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <Route
+              path="conta/*"
+              element={
+                <ProtectedRoute>
+                  <User />
+                </ProtectedRoute>
+              }
+            />
           </Routes>
           <Footer />
         </UserStorage>
--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -40,6 +40,8 @@ export const UserStorage = ({ children }) => {
         } finally {
           setLoading(false);
         }
+      } else {
+        setLogin(false);
       }
     }
     autoLogin();
