4d7dfa2883cc5b8f0971b7c3efcfeb4ad03f0fde
Luiz Otavio <luizotavio0106@outlook.com>
2022-09-08 17:25:26 -0300

    feat/user page header creation

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import UseHeader from './UseHeader';
+import UserPhotoPost from './UserPhotoPost';
+import UserStats from './UserStats';
+import Feed from '../Feed/Feed';
 
 const User = () => {
-  return <div>Usuário</div>;
+  return (
+    <section className="container">
+      <UseHeader />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="postar" element={<UserPhotoPost />} />
+        <Route path="estatisticas" element={<UserStats />} />
+      </Routes>
+    </section>
+  );
 };
 
 export default User;
