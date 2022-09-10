fec3bff0b48f55fb194feee6a72a6859e4118b97
Luiz Otavio <luizotavio0106@outlook.com>
2022-09-10 18:29:45 -0300

    feat/creation of the modal to display the details of each photo

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -7,13 +7,12 @@ import { PHOTOS_GET } from '../../api';
 
 import styles from './FeedPhotos.module.css';
 
-const FeedPhotos = () => {
+const FeedPhotos = ({ setModalPhoto }) => {
   const { data, loading, error, request } = useFetch();
   React.useEffect(() => {
     async function fetchPhotos() {
       const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
       const { json } = await request(url, options);
-      console.log(json);
     }
     fetchPhotos();
   }, [request]);
@@ -24,7 +23,11 @@ const FeedPhotos = () => {
     return (
       <ul className={`${styles.feed} animeLeft`}>
         {data.map((photo) => (
-          <FeedPhotosItem key={photo.id} photo={photo} />
+          <FeedPhotosItem
+            key={photo.id}
+            photo={photo}
+            setModalPhoto={setModalPhoto}
+          />
         ))}
       </ul>
     );
