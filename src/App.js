7acda3a76b1cf57d80dff55176899cd27f9bfc6c
Luiz Otavio <luizotavio0106@outlook.com>
2022-09-22 14:44:54 -0300

    feat/user profile page creation

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import Home from './Components/Home';
 import Login from './Components/Login/Login';
 import User from './Components/User/User';
 import Photo from './Components/Photo/Photo';
+import UserProfile from './Components/User/UserProfile';
+import ProtectedRoute from './Components/Helper/ProtectedRoute';
 import { UserStorage } from './UserContext';
 
 import './App.css';
-import ProtectedRoute from './Components/Helper/ProtectedRoute';
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
               }
             />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
           </Routes>
           <Footer />
         </UserStorage>
